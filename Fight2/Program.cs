c96afc3abdb43c6fabfbb9173531733d41260303
Suren Arakelyan <suren.arakelyan@elev.ga.ntig.se>
2023-10-20 13:41:25 +0200

    HEj2

--- Fight2/Program.cs
+++ Fight2/Program.cs
@@ -12,20 +12,20 @@ hero.weapon.name = "Boxar";
 
 Random generator = new Random();
 
-while (hero.Health > 0 && villainHp > 0)
+while (hero.Health > 0 && Villain.Health > 0)
 {
   Console.WriteLine("\n----- ===== NY RUNDA ===== -----");
   Console.WriteLine($"{hero.name}: {hero.Health}  {Villain.name}: {Villain.Health}\n");
 
   int heroDamage = generator.Next(30);
   Villain.Health -= heroDamage;
-  villainHp = Math.Max(0, villainHp);
-  Console.WriteLine($"{heroName} gör {heroDamage} skada på {villainName}");
+  Villain.Health = Math.Max(0, Villain.Health);
+  Console.WriteLine($"{hero.Health} gör {heroDamage} skada på {Villain.name}");
 
   int villainDamage = generator.Next(20);
-  heroHp -= villainDamage;
-  heroHp = Math.Max(0, heroHp);
-  Console.WriteLine($"{villainName} gör {villainDamage} skada på {heroName}");
+  hero.Health -= villainDamage;
+  hero.Health = Math.Max(0, hero.Health);
+  Console.WriteLine($"{Villain.Health} gör {villainDamage} skada på {hero.name}");
 
   Console.WriteLine("Tryck på vilken knapp du vill för att fortsätta.");
   Console.ReadKey();
@@ -33,17 +33,17 @@ while (hero.Health > 0 && villainHp > 0)
 
 Console.WriteLine("\n----- ===== STRIDEN ÄR SLUT ===== -----");
 
-if (heroHp == 0 && villainHp == 0)
+if (hero.Health == 0 && Villain.Health == 0)
 {
   Console.WriteLine("OAVGJORT");
 }
-else if (heroHp == 0)
+else if (hero.Health == 0)
 {
-  Console.WriteLine($"{villainName} vann!");
+  Console.WriteLine($"{Villain.name} vann!");
 }
 else
 {
-  Console.WriteLine($"{heroName} vann!");
+  Console.WriteLine($"{hero.name} vann!");
 }
 
 Console.WriteLine("Tryck på vilken knapp du vill för att fortsätta.");
